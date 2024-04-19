152ad1d8f5ef93123beb58b1bc2bd8bfd0622e66
sora <147121795+sora7716@users.noreply.github.com>
2024-04-19 15:59:24 +0900

    数字の予想から偶数と奇数だけの予想に変更 数字を間違えた時に出力される文字を追加 サイコロの出目を計算する関数を作成

--- PG3_02_03/main.cpp
+++ PG3_02_03/main.cpp
@@ -4,6 +4,12 @@
 #include <Windows.h>
 typedef void (*PFunc)();//関数ポインタ
 
+int DiceRoll() {
+	srand((unsigned int)time(nullptr));
+	int dice = rand() % 6 + 1;//サイコロの目
+	return dice;
+}
+
 void Correct() {
 	printf("正解\n");
 }
@@ -12,33 +18,43 @@ void Incorrect() {
 	printf("不正解\n");
 }
 
+void Mistake() {
+	printf("入力された数字が違います");
+}
+
 //コールバック関数
 void SetTimeout(PFunc p, int second) {
-	Sleep(second * 1000);
+	Sleep(second);
 	p();
 }
 
 int main() {
 	PFunc text;//関数ポインタ
 
-	srand((unsigned int)time(nullptr));
-	int dice = rand() % 6 + 1;//サイコロの目
+	int dice = DiceRoll();
 	int num  = 0;//予想される数
 
-	printf("予想される値を入力してください(1~6) : ");
+	printf("予想される値を入力してください : \n偶数:0,奇数:1\n");
 	scanf_s("%d", &num);
-	printf("%d\n", num);
 
 	//サイコロの目と予想される数が同じか↓
-	if (num == dice) {
-		text = Correct;
+	if (num == 0 || num == 1) {
+		if (num == 0 && dice % 2 == 0) {
+			text = Correct;
+		}
+		else if (num == 1 && dice % 2 == 1) {
+			text = Correct;
+		}
+		else {
+			text = Incorrect;
+		}
 	}
 	else {
-		text = Incorrect;
+		text = Mistake;
 	}
 	//サイコロの目と予想される数が同じか↑
 
-	SetTimeout(text, 3);//3秒後に結果を表示
+	SetTimeout(text, 3000);//3ミリ秒後に結果を表示
 	return 0;
 }
 
