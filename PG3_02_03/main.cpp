4c6a1f25e21eae87eea7461a859fcbf8fa3939e3
sora <147121795+sora7716@users.noreply.github.com>
2024-04-19 15:17:19 +0900

    関数ポインタとコールバック関数を用いてサイコロの出目をユーザーに当ててもらうゲームを作成しました。

--- PG3_02_03/main.cpp
+++ PG3_02_03/main.cpp
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+typedef void (*PFunc)(int *);//関数ポインタ
+
+void Correct(int *num) {
+	printf("%dで正解\n",*num);
+}
+
+void Incorrect(int *num) {
+	printf("%dで不正解\n",*num);
+}
+
+//コールバック関数
+void SetTimeout(PFunc p, int second,int num) {
+	Sleep(second * 1000);
+	p(&num);
+}
 
 int main() {
+	PFunc text;//関数ポインタ
 
+	srand((unsigned int)time(nullptr));
+	int dice = rand() % 6 + 1;//サイコロの目
+	int num  = 0;//予想される数
+
+	printf("予想される値を入力してください(1~6) : ");
+	scanf_s("%d", &num);
+	printf("%d\n", num);
+
+	//サイコロの目と予想される数が同じか↓
+	if (num == dice) {
+		text = Correct;
+	}
+	else {
+		text = Incorrect;
+	}
+	//サイコロの目と予想される数が同じか↑
+
+	SetTimeout(text, 3,num);//3秒後に結果を表示
 	return 0;
 }
 
